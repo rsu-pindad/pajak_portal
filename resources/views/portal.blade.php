f6c5932bd13a11eca43d9fe3cee503c2f43d58f1
rsu-pindad <sautomated01@gmail.com>
2025-01-15 09:07:20 +0700

    initial portal

---
+++ resources/views/portal.blade.php
@@ -0,0 +1,121 @@
+<x-guest-layout>
+
+  <section class="py-4 antialiased dark:bg-gray-900 md:py-6">
+    <div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
+      <div class="mb-4 flex items-center justify-between gap-4 md:mb-8">
+        <h2 class="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">Apps Lokal</h2>
+      </div>
+
+      <div class="grid grid-flow-row gap-4">
+        <a href="#"
+           class="flex items-center rounded-lg border border-gray-200 bg-white px-4 py-2 hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+          <svg class="me-2 h-4 w-4 shrink-0 text-gray-900 dark:text-white"
+               aria-hidden="true"
+               xmlns="http://www.w3.org/2000/svg"
+               width="24"
+               height="24"
+               fill="none"
+               viewBox="0 0 24 24">
+            <path stroke="currentColor"
+                  stroke-linecap="square"
+                  stroke-width="2"
+                  d="M8 15h7.01v.01H15L8 15Z" />
+            <path stroke="currentColor"
+                  stroke-linecap="square"
+                  stroke-width="2"
+                  d="M20 6H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1Z" />
+            <path stroke="currentColor"
+                  stroke-linecap="square"
+                  stroke-width="2"
+                  d="M6 9h.01v.01H6V9Zm0 3h.01v.01H6V12Zm0 3h.01v.01H6V15Zm3-6h.01v.01H9V9Zm0 3h.01v.01H9V12Zm3-3h.01v.01H12V9Zm0 3h.01v.01H12V12Zm3 0h.01v.01H15V12Zm3 0h.01v.01H18V12Zm0 3h.01v.01H18V15Zm-3-6h.01v.01H15V9Zm3 0h.01v.01H18V9Z" />
+          </svg>
+          <span class="text-nowrap text-sm font-medium uppercase text-gray-900 dark:text-white">
+            PPH 21
+          </span>
+        </a>
+        <a href="http://10.70.70.26:8080"
+           target="_blank"
+           class="flex items-center rounded-lg border border-gray-200 bg-white px-4 py-2 hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+          <svg class="me-2 h-4 w-4 shrink-0 text-gray-900 dark:text-white"
+               aria-hidden="true"
+               xmlns="http://www.w3.org/2000/svg"
+               width="24"
+               height="24"
+               fill="none"
+               viewBox="0 0 24 24">
+            <path stroke="currentColor"
+                  stroke-linecap="square"
+                  stroke-width="2"
+                  d="M8 15h7.01v.01H15L8 15Z" />
+            <path stroke="currentColor"
+                  stroke-linecap="square"
+                  stroke-width="2"
+                  d="M20 6H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1Z" />
+            <path stroke="currentColor"
+                  stroke-linecap="square"
+                  stroke-width="2"
+                  d="M6 9h.01v.01H6V9Zm0 3h.01v.01H6V12Zm0 3h.01v.01H6V15Zm3-6h.01v.01H9V9Zm0 3h.01v.01H9V12Zm3-3h.01v.01H12V9Zm0 3h.01v.01H12V12Zm3 0h.01v.01H15V12Zm3 0h.01v.01H18V12Zm0 3h.01v.01H18V15Zm-3-6h.01v.01H15V9Zm3 0h.01v.01H18V9Z" />
+          </svg>
+          <span class="text-nowrap text-sm font-medium uppercase text-gray-900 dark:text-white">
+            Barcode
+          </span>
+        </a>
+        <a href="http://10.70.70.26:8072"
+           target="_blank"
+           class="flex items-center rounded-lg border border-gray-200 bg-white px-4 py-2 hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+          <svg class="me-2 h-4 w-4 shrink-0 text-gray-900 dark:text-white"
+               aria-hidden="true"
+               xmlns="http://www.w3.org/2000/svg"
+               width="24"
+               height="24"
+               fill="none"
+               viewBox="0 0 24 24">
+            <path stroke="currentColor"
+                  stroke-linecap="square"
+                  stroke-width="2"
+                  d="M8 15h7.01v.01H15L8 15Z" />
+            <path stroke="currentColor"
+                  stroke-linecap="square"
+                  stroke-width="2"
+                  d="M20 6H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1Z" />
+            <path stroke="currentColor"
+                  stroke-linecap="square"
+                  stroke-width="2"
+                  d="M6 9h.01v.01H6V9Zm0 3h.01v.01H6V12Zm0 3h.01v.01H6V15Zm3-6h.01v.01H9V9Zm0 3h.01v.01H9V12Zm3-3h.01v.01H12V9Zm0 3h.01v.01H12V12Zm3 0h.01v.01H15V12Zm3 0h.01v.01H18V12Zm0 3h.01v.01H18V15Zm-3-6h.01v.01H15V9Zm3 0h.01v.01H18V9Z" />
+          </svg>
+          <span class="text-nowrap text-sm font-medium uppercase text-gray-900 dark:text-white">
+            Bukti Potong (Bandung)
+          </span>
+        </a>
+        <a href="http://10.70.70.26:8066"
+           target="_blank"
+           class="flex items-center rounded-lg border border-gray-200 bg-white px-4 py-2 hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+          <svg class="me-2 h-4 w-4 shrink-0 text-gray-900 dark:text-white"
+               aria-hidden="true"
+               xmlns="http://www.w3.org/2000/svg"
+               width="24"
+               height="24"
+               fill="none"
+               viewBox="0 0 24 24">
+            <path stroke="currentColor"
+                  stroke-linecap="square"
+                  stroke-width="2"
+                  d="M8 15h7.01v.01H15L8 15Z" />
+            <path stroke="currentColor"
+                  stroke-linecap="square"
+                  stroke-width="2"
+                  d="M20 6H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1Z" />
+            <path stroke="currentColor"
+                  stroke-linecap="square"
+                  stroke-width="2"
+                  d="M6 9h.01v.01H6V9Zm0 3h.01v.01H6V12Zm0 3h.01v.01H6V15Zm3-6h.01v.01H9V9Zm0 3h.01v.01H9V12Zm3-3h.01v.01H12V9Zm0 3h.01v.01H12V12Zm3 0h.01v.01H15V12Zm3 0h.01v.01H18V12Zm0 3h.01v.01H18V15Zm-3-6h.01v.01H15V9Zm3 0h.01v.01H18V9Z" />
+          </svg>
+          <span class="text-nowrap text-sm font-medium uppercase text-gray-900 dark:text-white">
+            Bukti Potong (Turen)
+          </span>
+        </a>
+      </div>
+    </div>
+  </section>
+
+</x-guest-layout>
